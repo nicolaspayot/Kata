1f4160b9613213b0c88027e0a2b392986e6d1825
Nicolas Payot <nicolas.payot@gmail.com>
2015-04-03 19:33:20 +0200

    test: add books to cart

--- HarryPotter/features/step_definitions/shopping-cart.steps.js
+++ HarryPotter/features/step_definitions/shopping-cart.steps.js
@@ -3,21 +3,23 @@
 var assert = require('assert');
 var scriptPath = process.cwd() + '/script';
 var ShoppingCart = require(scriptPath + '/shopping-cart');
+var Book = require(scriptPath + '/book');
 
 module.exports = function() {
 
   var cart;
 
   this.Given(/^I have an empty cart$/, function (callback) {
-    // Write code here that turns the phrase above into concrete actions
     cart = new ShoppingCart();
     assert.equal(cart.books.length, 0);
     callback();
   });
 
   this.When(/^I add (\d+) book to the cart$/, function (arg1, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    for (var i = 0; i < Number(arg1); ++i) {
+      cart.addBook(new Book("Harry Potter, volume 1"));
+    }
+    callback();
   });
 
   this.Then(/^The cart contains (\d+) book$/, function (arg1, callback) {
