ff479111f9319c642573ba911fb25262e52220cc
Nicolas Payot <nicolas.payot@gmail.com>
2015-04-03 19:49:49 +0200

    test: change empty cart check

--- HarryPotter/features/step_definitions/shopping-cart.steps.js
+++ HarryPotter/features/step_definitions/shopping-cart.steps.js
@@ -11,7 +11,7 @@ module.exports = function() {
 
   this.Given(/^I have an empty cart$/, function (callback) {
     cart = new ShoppingCart();
-    assert.equal(cart.books.length, 0);
+    assert.equal(cart.isEmpty(), true);
     callback();
   });
 
