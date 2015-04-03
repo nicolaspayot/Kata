1dcb81221da61d2fbcf5045cfb80b4da4d0010cd
Nicolas Payot <nicolas.payot@gmail.com>
2015-04-03 19:21:36 +0200

    test: empty cart

--- HarryPotter/features/step_definitions/shopping-cart.steps.js
+++ HarryPotter/features/step_definitions/shopping-cart.steps.js
@@ -1,15 +1,18 @@
 'use strict';
 
+var assert = require('assert');
 var scriptPath = process.cwd() + '/script';
 var ShoppingCart = require(scriptPath + '/shopping-cart');
 
 module.exports = function() {
 
-  var cart = new ShoppingCart();
+  var cart;
 
   this.Given(/^I have an empty cart$/, function (callback) {
     // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    cart = new ShoppingCart();
+    assert.equal(cart.books.length, 0);
+    callback();
   });
 
   this.When(/^I add (\d+) book to the cart$/, function (arg1, callback) {
