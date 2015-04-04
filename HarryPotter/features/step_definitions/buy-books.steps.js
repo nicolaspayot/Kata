2ad16a7d949be92d2bb9dcdb185e57d47489d241
Nicolas Payot <nicolas.payot@gmail.com>
2015-04-04 13:51:48 +0200

    test: cart total price for 1 book

--- HarryPotter/features/step_definitions/buy-books.steps.js
+++ HarryPotter/features/step_definitions/buy-books.steps.js
@@ -1,19 +1,28 @@
 'use strict';
 
+var assert = require('assert');
+var scriptPath = process.cwd() + '/script';
+var ShoppingCart = require(scriptPath + '/shopping-cart');
+var Book = require(scriptPath + '/book');
+
 module.exports = function() {
 
+  var cart;
+  var book;
+
   this.Given(/^I want to buy "([^"]*)"$/, function (arg1, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    book = new Book(arg1);
+    callback();
   });
 
   this.When(/^I add it to the cart$/, function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    cart = new ShoppingCart();
+    cart.addBook(book);
+    callback();
   });
 
   this.Then(/^I must pay (\d+) euros$/, function (arg1, callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    assert.equal(cart.getTotal(), 8);
+    callback();
   });
 };
