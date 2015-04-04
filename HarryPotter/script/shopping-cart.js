c04490221cf53c84aedb24bfba090edeb33621eb
Nicolas Payot <nicolas.payot@gmail.com>
2015-04-04 17:34:58 +0200

    feat: total with discount

--- HarryPotter/script/shopping-cart.js
+++ HarryPotter/script/shopping-cart.js
@@ -1,6 +1,9 @@
-'use strict';
+  'use strict';
 
 module.exports = function() {
+
+  var discounts = { 2: 0.05, 3: 0.1, 4: 0.2, 5: 0.25 };
+
   var books = [];
 
   this.isEmpty = function() {
@@ -15,9 +18,31 @@ module.exports = function() {
     return books.length;
   };
 
+  var getBookCount = function() {
+    var bookCount = {};
+    books.forEach(function(book) {
+      var count = bookCount[book.title];
+      if (typeof count === 'undefined') {
+        bookCount[book.title] = 0;
+      }
+      ++bookCount[book.title];
+    });
+    return bookCount;
+  };
+
   this.getTotal = function() {
-    return books.reduce(function(total, book) {
-      return total + book.price;
+    var bookCount = getBookCount();
+
+    var titles = Object.keys(bookCount);
+    var total = titles.reduce(function(total, title) {
+      return total + bookCount[title] * 8.0;
     }, 0);
+
+    var discount = discounts[titles.length];
+    if (discount) {
+      total -= total * discount;
+    }
+
+    return total;
   };
 };
